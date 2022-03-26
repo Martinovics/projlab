a0c300dec100004ee8aca075ed96e60bca0be04b
Csabesz1123 <csa.balogh.0165@gmail.com>
2022-03-26 15:51:01 +0100

    Csaba - Zsák

--- src/Szkeleton/Zsák.java
+++ src/Szkeleton/Zsák.java
@@ -1,18 +1,31 @@
 package Szkeleton;
 
 public class Zsák extends Item{
+    /**
+    *-9 férőhelyet “ad” a virológusnak.
+    *@param v - paraméter virológus
+    *@return void
+    */
     @Override
     public void AntiEffekt(Virologus v) {
-        super.AntiEffekt(v);
+        v.Ágens[].size-=9;
     }
-
+    /**
+    *A TárgyMegkapás() végén hívódik, amikor a Virológus felvette az Itemet. A zsák +9 férőhelyet ad a virológusnak.
+    *@param v - paraméter virológus
+    *@return void
+    */
     @Override
     public void Effekt(Virologus v) {
-        super.Effekt(v);
+        v.Ágens[].size+=9;
     }
-
+    /**
+    *Meghívódáskor “a” ágenssel tér vissza.
+    *@param v - paraméter virológus
+    *@return a - ágens
+    */
     @Override
     public Ágens BekendőésEffket(Virologus v, Ágens a) {
-        return super.BekendőésEffket(v, a);
+        return a;
     }
 }
