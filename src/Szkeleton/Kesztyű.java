1441027b43ed079a79b277449f919a8746c31e82
Csabesz1123 <csa.balogh.0165@gmail.com>
2022-03-26 15:29:27 +0100

    Csaba Itemshez

--- src/Szkeleton/Kesztyű.java
+++ src/Szkeleton/Kesztyű.java
@@ -1,19 +1,36 @@
 package Szkeleton;
 
 public class Kesztyű extends Item{
-
+     /**
+    *Nem csinál semmi érdemlegeset.
+    *@param v - paraméter virológus
+    *@return void
+    */
     @Override
     public void AntiEffekt(Virologus v) {
-        super.AntiEffekt(v);
+        Item.AntiEffekt(v);
+        v.TárgyElvétel(v, this);
+        
     }
-
+    /**
+    *A TárgyMegkapás() végén hívódik, amikor a Virológus felvette az Itemet.
+    *A kesztyű Effekt metódusa nem csinál semmi érdemlegeset.
+    *@param v - paraméter virológus
+    *@return void
+    */
     @Override
     public void Effekt(Virologus v) {
-        super.Effekt(v);
+        Item.Effekt(v);
     }
-
+     /**
+    *A paraméterként kapott virológusra keni vissza az "a" ágenst, s Nullágenssel tér vissza.
+    *@param v - paraméter virológus
+    *@param a - paraméterként kapott ágens
+    *@return Nullágens
+    */
     @Override
     public Ágens BekendőésEffket(Virologus v, Ágens a) {
-        return super.BekendőésEffket(v, a);
+        v.BekendőésEffket(v, a);
+        return NullÁgens;
     }
 }
--- src/Szkeleton/Köpeny.java
+++ src/Szkeleton/Köpeny.java
@@ -1,18 +1,43 @@
 package Szkeleton;
 
+import java.util.Random;
+
+        /*
+        * 82.3%-al ad vissza NullÁgens-t, a többi esetben a paraméterként kapott a-t.
+        @param v - paramáterként kapott virológus
+        @param a - paraméterként kapott ágens
+        *
+        @return
+        */
 public class Köpeny extends Item{
     @Override
     public Ágens BekendőésEffket(Virologus v, Ágens a) {
-        return super.BekendőésEffket(v, a);
+        Random rand = new Random();
+        rand.nextDouble();
+        if(rand<=0.823) {
+            return NullÁgens;
+        }
+        else {
+            return a;
+        }
     }
-
+    /**
+    *A TárgyMegkapás() végén hívódik, amikor a Virológus felvette az Itemet.
+    *A kesztyű Effekt metódusa nem csinál semmi érdemlegeset.
+    *@param v - paraméter virológus
+    *@return void
+    */
     @Override
     public void Effekt(Virologus v) {
-        super.Effekt(v);
+        Item.Effekt(v);
     }
-
+     /**
+    *Nem csinál semmi érdemlegeset.
+    *@param v - paraméter virológus
+    *@return void
+    */
     @Override
     public void AntiEffekt(Virologus v) {
-        super.AntiEffekt(v);
+        Item.AntiEffekt(v);
     }
 }
