eb56ae119bca7d0dee12cb1a90498342568e0682
Csabesz1123 <csa.balogh.0165@gmail.com>
2022-03-26 16:32:01 +0100

    Csaba - Item, VédőÁgens

--- src/Szkeleton/VédőÁgens.java
+++ src/Szkeleton/VédőÁgens.java
@@ -1,18 +1,31 @@
 package Szkeleton;
 
 public class VédőÁgens extends Ágens{
+    /**
+    *NullÁgenssel tér vissza
+    *@param v - virológus
+    *@return ágens
+    */
     @Override
     public Ágens BekendőésEffekt(Ágens a) {
-        return super.BekendőésEffekt(a);
+        return NullÁgenssel;
     }
-
+    /**
+    *Lásd:Ágens. + Semlegesít minden ágenst, amit a körben és a következő egy körben  a Virológusra kentek.
+    *@param v - virológus
+    *@return void
+    */
     @Override
     public void Effekt(Virologus v) {
-        super.Effekt(v);
+        Item.Effekt(v);
     }
-
+    /**
+    *Nem csinál semmit.
+    *@param v - virológus
+    *@return void
+    */
     @Override
     public void AntiEffekt(Virologus v) {
-        super.AntiEffekt(v);
+
     }
 }
