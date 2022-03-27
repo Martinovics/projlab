51da38690014a2694fff155a47ba248d55b823a1
davidbalogh420 <dave.balogh02@gmail.com>
2022-03-27 18:36:24 +0200

    commit

--- src/Szkeleton/Virologus.java
+++ src/Szkeleton/Virologus.java
@@ -4,7 +4,9 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class Virologus implements Steppable {
-
+    /**@author Balogh Dávid
+     * A virológus attribútumai
+     * */
     public ArrayList<Anyag> anyagok;
     public ArrayList<Ágens> kódok;
     public ArrayList<Ágens> ágensek;
@@ -13,58 +15,97 @@ public class Virologus implements Steppable {
     public ArrayList<Item> felszerelés;
     public ILépés LépésViselkedés;
     private ArrayList<Cucc> rabolható;
-
-
     public int itemCapacity;
 
-
-
+    /**
+     * @author Balogh Dávid
+     * A virológus mozgatásáért felelős függvény
+     * Kiszedi az egyik mezőről és berakja a másikba
+     * @param m Mező
+     * */
     public void move(Mező m){
 
         m.AcceptViro(this);
         this.mező.RemoveViro(this);
 
     }
+    /**
+     *@author Balogh Dávid
+     * Ez a függvény is a mozgatáshoz kell
+     *@param m Mező
+     * */
     public void Letapogat(Mező m){
         m.Felfedez(this);
     }
-
+    /**
+     * @author Balogh Dávid
+     * Az Ágensek elkészítését meghívó függvény
+     * @param a Ágens
+     * */
     public void ÁgensElőállít(Ágens a){
         a.Create(this);
 
     }
-
+    /**
+     * @author Balogh Dávid
+     * Valamilyen dolgot egy mezőről felveszünk és eltárolunk
+     * */
     public void CuccFelvétel(){
         this.mező.CuccÁtadás(this);
     }
 
+    /**
+     * @author Balogh Dávid
+     * Valamilyen kódot eltárolunk
+     * @param a Ágens
+     * */
     public boolean GénMegkapás(Ágens a){
         this.kódok.add(a);
         return true;
     }
+    /**
+     * @author Balogh Dávid
+     * Valamilyen anyagot eltárolunk
+     * @param a Anyag
+     * */
     public boolean AnyagMegkapás(Anyag a){
         this.anyagok.add(a);
         return true;
     }
+    /**
+     * @author Balogh Dávid
+     * Valamilyen tárgyat eltárolunk
+     * @param i Item
+     * */
     public boolean TárgyMegkapás(Item i){
         this.felszerelés.add(i);
         i.Effekt(this);
         return true;
     }
+    /**
+     * @author Balogh Dávid
+     * Valamilyen kész ágenst eltárolunk
+     * */
     public boolean ÁgensMegkapás(Ágens a){
         this.ágensek.add(a);
 
         return true;
     }
-
+    /**
+     * @author Balogh Dávid
+     * Valamilyen ágens hatást aktiválunk, tehát szintén
+     * kollekcióba eltárolunk
+     * */
     public boolean BuffMegkapás(Ágens a){
         this.buff.add(a);
         a.Effekt(this);
         return true;
     }
-
-
-
+    /**
+     * @author Balogh Dávid
+     * Valamilyen ágens hatást deaktiválunk, tehát szintén
+     * kollekcióból kiszedünk
+     * */
     public void RemoveBuff(Ágens a){
         a.AntiEffekt(this);
         this.buff.remove(a);
@@ -72,9 +113,17 @@ public class Virologus implements Steppable {
 
 
 
+    /**
+     * @author Balogh Dávid
+     * Valamilyen dolgot kiszedünk a kollekciónkból
+     * @param c Cucc
+     * */
 
     public void RemoveCucc(Cucc c){
-        try{
+
+        c.RemoveCucc(this);
+
+        /*try{
           this.felszerelés.remove(c);
         }
         catch (Exception e){
@@ -86,10 +135,16 @@ public class Virologus implements Steppable {
         }
         catch (Exception e){
 
-        }
+        }*/
 
     }
 
+
+    /**
+     *@author Balogh Dávid
+     * Ez a függvény indítja el a rablás folyamatát
+     * @param v Virologus
+     * */
     public void Rabol(Virologus v){
 
 
@@ -110,7 +165,10 @@ public class Virologus implements Steppable {
 
     }
 
-
+    /**
+     * @author Balogh Dávid
+     * Visszatérés a rabolható tárgyak és anyagok kollekciójával
+     * */
     public  ArrayList<Cucc> Rabolva(){
         /*
         Cucc[] tomb_1= (Cucc[]) this.felszerelés.toArray();
@@ -138,6 +196,12 @@ public class Virologus implements Steppable {
 
     }
 
+    /**
+     * @author Balogh Dávid
+     *Itt vesszük ki az adott virológus kollekciójából a dolgokat
+     *@param v Virologus
+     *@param c Cucc
+     * */
     public  void TárgyElvétel(Virologus v, Cucc c){
 
         Item i = new Item();
@@ -161,11 +225,19 @@ public class Virologus implements Steppable {
 
     }
 
-
+    /**
+     * @author Balogh Dávid
+     * Kinullázzuk a virológus megtanult kódjainak a kollekcióját
+     * */
     public void mindentElfelejt(){
         this.kódok.removeAll(kódok);
     }
 
+
+    /**
+     * @author Balogh Dávid
+     * LépésViselkedéstől függően lépünk a virológussal
+     * */
     public void Step(){
 
 
@@ -200,19 +272,33 @@ public class Virologus implements Steppable {
        }*/
 
     }
-
+    /**
+     * @author Balogh Dávid
+     * Átállítjuk a LépésViselkedésünket
+     * @param s ILépés
+     * */
     public void setLépésBehaviour(ILépés s){
         this.LépésViselkedés=s;
     }
 
 
-
+    /**
+     * @author Balogh Dávid
+     * ELindítja a kenés folyamatát
+     * @param v Virologus
+     * @param a Ágens
+     * */
     public void ÁgensKenés(Virologus v, Ágens a){
         this.ágensek.remove(a);
         Bekenődés(this,a);
     }
 
-
+    /**
+     * @author Balogh Dávid
+     * Bekenjük az adott virológust és aktiváljuk az effektet
+     * @param v Virologus
+     * @param a Ágens
+     * */
     public void Bekenődés(Virologus v, Ágens a){
 
 
@@ -233,17 +319,29 @@ public class Virologus implements Steppable {
 
 
 
-
+    /**
+     * @author Balogh Dávid
+     * A kenés védekezést és visszadobást valósítja meg
+     * @param a Ágens
+     * */
     public void overwhelmingBekenődés(Ágens a){
         a.BuffÁtadás(this);
 
     }
+
+    /**
+     * @author Balogh Dávid
+     * Kör vége
+     * */
     public void EndRound(){
 
         System.out.println("Endround meghívva");
     }
 
-
+    /**
+     * @author Balogh Dávid
+     * Visitek
+     * */
     public void visit(Mező m){
         System.out.println("Mező ősosztály visitelve lett");
     }
